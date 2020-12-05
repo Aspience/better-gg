4aadeeb8f9ed1b23595c8dc756d87dba9b393da0
Aspience <aspience@gmail.com>
2020-12-05 05:58:07 +0300

    useState hook init

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,17 +1,18 @@
 import React from 'jsx-dom';
 
 import { FC } from '@core/types';
+import { useState } from '@core/hooks/useState';
 import { Modal } from '@src/shared/components/modal';
 import { ModuleItem } from '@src/shared/components/moduleItem';
-import { modules } from '@src/modules';
 
+import { modules } from '@src/modules';
 import { initMenu } from './utils/initMenu';
 import styles from './styles.scss';
 
 export const App: FC = () => {
-  const isOpened = false;
-  const show = () => {};
-  const hide = () => {};
+  const [isOpened, setIsOpened] = useState(false);
+  const show = () => setIsOpened(true);
+  const hide = () => setIsOpened(false);
 
   initMenu({ onClick: show });
 
